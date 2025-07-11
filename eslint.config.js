d3252aa9e3345cc1834f23303d02304c9656b9ff
mohammedyh <mohammedyousufhamid@gmail.com>
2025-07-11 17:20:11 +0100

    feat: migrate to eslint flat config

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+
+export default defineConfig([
+	globalIgnores(['dist']),
+	{
+		files: ['**/*.{js,jsx}'],
+		extends: [
+			js.configs.recommended,
+			reactHooks.configs['recommended-latest'],
+			reactRefresh.configs.vite,
+		],
+		languageOptions: {
+			ecmaVersion: 2020,
+			globals: globals.browser,
+			parserOptions: {
+				ecmaVersion: 'latest',
+				ecmaFeatures: { jsx: true },
+				sourceType: 'module',
+			},
+		},
+		rules: {
+			'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+		},
+	},
+	eslintPluginPrettier,
+]);
